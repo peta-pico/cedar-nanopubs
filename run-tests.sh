097187fb3e2905b59fa0bfe2156d8823a86c798c
Tobias Kuhn <kuhntobias@gmail.com>
2019-08-30 09:18:46 +0200

    Add HTML files

--- run-tests.sh
+++ run-tests.sh
@@ -6,4 +6,5 @@ for F in *.jsonld; do
   F="${F%.jsonld}"
   np op import -t cedar -o ../pre/$F.trig $F.jsonld
   np mktrusty -o ../nanopubs/$F.trig ../pre/$F.trig
+  np html -s -o ../html/$F.html ../nanopubs/$F.trig
 done
